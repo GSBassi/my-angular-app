125ebb453ff5cdd975305a89859687d4ddf15cd3
Gurinder Singh Bassi <bassi4.gs@gmail.com>
2019-05-16 21:06:25 -0400

    Recipe Detials section

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,7 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
- recipes: Recipe[]=[];
+ recipes: Recipe[]=[
+   new Recipe('A Test Recipe','This is simply a test','https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false'),new Recipe('A Test Recipe','This is simply a test','https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false')
+
+ ];
   constructor() { }
 
   ngOnInit() {
